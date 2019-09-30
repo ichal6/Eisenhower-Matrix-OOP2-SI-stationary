f8ba09f142bdc10122a936086884ca0de4129d4c
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-09-30 11:55:14 +0200

    Write skeleton maethod and constructor TodoItem

--- eisenhower/TodoItem.java
+++ eisenhower/TodoItem.java
@@ -1,5 +1,34 @@
 package eisenhower;
 
+import java.time.LocalDate;
 public class TodoItem{
-    
+    String title;
+    LocalDate deadline;
+    boolean isDone = false;
+
+    TodoItem(String title, LocalDate deadline){
+
+    }
+
+    private String getTitle(){
+        //This method must be return title String
+        return title;
+    }
+
+    private LocalDate getDeadline() {
+        return deadline;
+    }
+
+    private void mark(){
+        isDone = true;
+    }
+
+    private void unmark(){
+        isDone = false;
+    }
+
+    public String toString(){
+        String forString = "";
+        return forString;
+    }
 }

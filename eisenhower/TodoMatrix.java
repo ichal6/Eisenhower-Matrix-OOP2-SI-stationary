efc684dfbf0f9583c2d13c63adb95836662f5738
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-09-30 13:06:47 +0200

    Write skeleton method and constructor TodoMatrix

--- eisenhower/TodoMatrix.java
+++ eisenhower/TodoMatrix.java
@@ -1,5 +1,47 @@
 package eisenhower;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+
 public class TodoMatrix{
-    
+    HashMap<String, LocalDate> todoQuarters = new HashMap<String, LocalDate>();
+
+    TodoMatrix(){
+
+    }
+
+    private String getQuarter(String status){
+        //Returns a choosen object drom map. Status should be foreach(IU,IN,NU,NN)
+        return "";
+    }
+
+    private void addItem(String title, LocalDate deadline){
+        //Add new item to map
+    }
+
+    private void addItem(String title, LocalDate deadline, boolean isImportant){
+        //Add new item to map
+    }
+
+    private void addItemsFromFile(String filename){
+        //Add items from file to map by format:
+        // title|day-month|is_important
+    }
+
+    private void saveItemFromFile(String filename){
+        //Add items from mat to file by format:
+        // title|day-month|is_important
+    }
+
+    private void archiveItems(){
+        //Removes all TodoItem object with parametr isDone from list todoItems
+    }
+
+    public String toString(){
+        //Return a todoQuarters list formatted to string
+        String returnString = "";
+        return returnString;
+    }
+
+
 }

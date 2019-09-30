bb67d6af2e84e8e8ad54ddfbd961d350efdc5903
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-09-30 12:11:32 +0200

    Write skeleton maethod and constructor TodoQuarter

--- eisenhower/TodoQuarter.java
+++ eisenhower/TodoQuarter.java
@@ -1,5 +1,38 @@
 package eisenhower;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 public class TodoQuarter{
-    
+    ArrayList<String> toDoItems = new ArrayList<String>();
+
+    TodoQuarter(){
+
+    }
+
+    private void addItem(String title, LocalDate deadLine){
+        //Append new item to List
+    }
+
+    private void removeItem(int index){
+        //Remove object with index from list
+    }
+
+    private void archiveItems(){
+        //Remove all object with parametr isDone == true from list
+    }
+
+    private String getItem(int index){
+        String object = "";
+        return object;
+    }
+
+    private void/*uncertain return type */ getItems(){ 
+        //Return private field todoItems - It's uncertain
+    }
+
+    public String toString(){
+        // Return formatted list to label 
+        String forString = "";
+        return forString;
+    }
 }

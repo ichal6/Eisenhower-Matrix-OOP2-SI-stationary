aa58801bfcabf5cea4769e4cc020bcf67c9bb227
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-10-01 16:13:07 +0200

    Improve constructor in TodoMatrix

--- TodoMatrix.java
+++ TodoMatrix.java
@@ -6,7 +6,10 @@ public class TodoMatrix{
     private HashMap<String, TodoQuarter> todoQuarters = new HashMap<String, TodoQuarter>();
 
     TodoMatrix(){
-
+        todoQuarters.put("IU", new TodoQuarter());
+        todoQuarters.put("IN", new TodoQuarter());
+        todoQuarters.put("NU", new TodoQuarter());
+        todoQuarters.put("NN", new TodoQuarter());
     }
     public Map<String, TodoQuarter> getQuarters(){
         return todoQuarters;

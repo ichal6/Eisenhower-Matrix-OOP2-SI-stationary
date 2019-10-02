1bfd400d61a1a436e76b6f0efa8d69f9e5c81580
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-10-02 13:29:53 +0200

    Create Menu class

---
+++ Menu.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Menu {
+    Menu(){
+
+    }
+    public void displayMenu(){
+        
+        String[] options =  {"1. Show quarter matrix", "2. Add item to matrix",
+                            "3. Mark or unmark your tasks", "4. Remove items from matrix",
+                            "5. Remove all done items", "6. Save to file items",
+                            "7. Remove all done items, save to file and close program",
+                            "8. Show all matrix", "0. Close program"};
+        for(String option: options){
+           System.out.println(option); 
+        }
+        
+        int numberOfOption = chooseOption();
+        
+
+
+    }
+
+    private int chooseOption(){
+        int numberOfOption = 0;
+        boolean isInsert = true;
+        while(isInsert){
+            System.out.println("Please choose option:");
+            try{
+                numberOfOption = Integer.parseInt(inputUserData());
+                isInsert = false;
+                
+            }catch(Exception e){
+                System.out.println("Incorrect value!");
+                isInsert = true;
+            }
+        }
+        return numberOfOption;
+    }
+
+    private String inputUserData(){
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        return text;
+    }
+}
